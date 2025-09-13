978275031d357d7a7b989b49c35d3a8a4874c0f0
AgustinPodhainy <apodhainyvignola@frba.utn.edu.ar>
2025-09-13 16:58:48 -0300

    subo df

--- glovo.py
+++ glovo.py
@@ -1,1 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
 
+glovo_datos = pd.read_excel("/content/drive/MyDrive/Colab Notebooks/Simulacion/Datos/FINAL_HORAS_NOVIEMBRE_Nacional_DF.xlsx", sheet_name='Datos')
---
+++ tpSimu.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from scipy import stats
+import matplotlib.pyplot as plt
+import re
